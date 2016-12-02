252cd07f4b735067fe73b0912fa410519b8f16f1
FrankieMFerreira <FrankieMFerreira@gmail.com>
2016-12-02 13:02:09 -0500

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,29 @@
+class Author
+
+  attr_accessor :name, :posts
+
+  @@post_count = 1
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+
+  def add_post(post)
+    @posts << post
+    post.author = self
+  end
+
+  def add_post_by_title(title)
+    newpost = Post.new(title)
+    self.add_post(newpost)
+    @@post_count += 1
+  end
+
+  def self.post_count
+  @@post_count
+  end
+
+
+end
